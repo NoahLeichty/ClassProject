8cd468f870b8af4837507c6a03d3543bd3205863
Noah Leichty <nleichty@millikin.edu>
2025-03-13 21:18:31 -0500

    Added methods for breakfast, lunch, and dinner.

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,27 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
+        // Methods
+        breakfast();
+        brunch();
+        lunch();
+        dinner();
+    }
+    // Method for breakfast
+    private static void breakfast(){
 
-        }
+    }
+    // Method for brunch recipes
+    private static void brunch(){
+
+    }
+    // Method for lunch recipes
+    private static void lunch(){
+
+    }
+    // Method for dinner recipes
+    private static void dinner(){
+
     }
 }
